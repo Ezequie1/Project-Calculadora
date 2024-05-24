e6c62e8da076d68b3baa1e9cdabe35ed8e20bf09
Ezequiel <ezequielalves1021@gmail.com>
2024-05-24 15:44:06 -0300

    Add logic and resolving dependecies

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -2,50 +2,25 @@ import './App.css';
 import SignalCellularAltRoundedIcon from '@mui/icons-material/SignalCellularAltRounded';
 import WifiRoundedIcon from '@mui/icons-material/WifiRounded';
 import battery from './assets/battery.png'
-import { useEffect, useState } from 'react';
-
+import { useState } from 'react';
 
 export default function App() {
 
   const [time, updateTime] = useState([])
-  setTimeout(() => { updateTime( new Date().toTimeString().split(':'))}, 1000)
-  const [font, setFont] = useState('100px')
-  const [clean, setClean] = useState('AC')
-  const [firstValue, setFirstValue] = useState(null)
+  setTimeout(() => { updateTime( new Date().toTimeString().split(':'))}, 500)
+  const [font, setFont] = useState('5.8rem')
+  const [firstValue, setFirstValue] = useState(0)
   const [oneFiled, setOne] = useState(false)
-  const [secondValue, setSecondValue] = useState(null)
+  const [secondValue, setSecondValue] = useState(0)
   const [twoFiled, setTwo] = useState(false)
   const [displayValue, setDisplayValue] = useState('0')
   const [operation, setOperation] = useState(null)
 
-  useEffect(() => {
-    switch(displayValue.length){
-      case 7:
-        setFont('90px')
-      break;
-      case 8:
-        setFont('80px')
-      break;
-      case 9: 
-        setFont('70px')
-      break;
-      default:
-        setFont('100px')
-      break;
-    }
-    displayValue !== '0' ? setClean('C') : setClean('AC')
-  })
-
   function addValue(value){
     if(displayValue.length < 9){
       if(!oneFiled){
-        if(displayValue === '0'){
-          setDisplayValue(value)
-          setFirstValue(Number(value))
-        }else{
-          setDisplayValue(displayValue + value)
-          setFirstValue(Number(displayValue + value))
-        }
+        setDisplayValue(displayValue === '0' ? value : displayValue + value)
+        setFirstValue(Number(displayValue === '0' ? value : displayValue + value))
       }else{
         if(!twoFiled){
           setDisplayValue(value)
@@ -56,49 +31,43 @@ export default function App() {
           setSecondValue(Number(displayValue + value))
         }
       }
-    } 
-  }
 
-  function changeNumbersVariations(variation){
-    if(!oneFiled){
-      if(variation === '+/-'){
-        if(Array.from(displayValue)[0] === '-'){
-          setDisplayValue(displayValue.substring(1))
-          setFirstValue(Number(displayValue.substring(1)))
-        }else{
-          setDisplayValue('-' + displayValue)
-          setFirstValue(Number('-' + displayValue))
-        }
-      }else{
-        setDisplayValue(displayValue + '%')
+      switch(displayValue.length - (displayValue.split('.').length - 1)){
+        case 7:
+          setFont('5.2rem')
+        break;
+        case 8:
+          setFont('4.6rem')
+        break;
+        case 9: 
+          setFont('4rem')
+        break;
+        default:
+          setFont('5.8rem')
+        break;
       }
-    }else{
-    }
-  }
 
-  function addOperation(operator){
-    if(!oneFiled && !twoFiled){
-      setOperation(operator)
-      setOne(true)
-    }else{
-      getResult()
+      //console.log(`FirstValue: ${firstValue} isFiled: ${oneFiled}`)
+      //console.log(`SecondValue: ${secondValue} isFiled: ${twoFiled}`)
+      //console.log(`Operation: ${operation}`)
+      //console.log(`DisplayValue: ${displayValue}`)
     }
   }
 
   function removeValues(){
-    if(clean === 'C'){
+    if(document.getElementById('cleanValue').innerHTML === 'C'){
       displayValue.length === 1 ? setDisplayValue('0') : setDisplayValue(displayValue.slice(0, -1));
     }else{
       setDisplayValue('0')
-      setFirstValue(null)
+      setFirstValue(0)
       setOne(false)
-      setSecondValue(null)
+      setSecondValue(0)
       setTwo(false)
     }
   }
 
   function getResult(){
-    if(firstValue !== null && operation !== null && Number(displayValue) !== null){
+    if(firstValue !== null && operation !== null){
       switch(operation){
         case 'x':
           setDisplayValue(String(firstValue * secondValue))
@@ -107,7 +76,6 @@ export default function App() {
         case '/':
           setDisplayValue(String(firstValue / secondValue))
           setFirstValue(firstValue / secondValue)
-
         break;
         case '+': 
           setDisplayValue(String(firstValue + secondValue))
@@ -120,8 +88,49 @@ export default function App() {
         default:
         break;
       }
-      setSecondValue(null)
-      setTwo(false) 
+
+      setSecondValue(0)
+      setTwo(false)
+      setOperation(null)
+    }
+
+  }
+
+  function changePositiveOrNegative(){
+    if(!oneFiled){
+        if(Array.from(displayValue)[0] === '-'){
+          setDisplayValue(displayValue.substring(1))
+          setFirstValue(Number(displayValue.substring(1)))
+        }else{
+          setDisplayValue('-' + displayValue)
+          setFirstValue(Number('-' + displayValue))
+        }
+    }else{
+      if(Array.from(displayValue)[0] === '-'){
+        setDisplayValue(displayValue.substring(1))
+        setSecondValue(Number(displayValue.substring(1)))
+      }else{
+        setDisplayValue('-' + displayValue)
+        setSecondValue(Number('-' + displayValue))
+      }
+    }
+  }
+
+  function porcentageValue(){
+    if(!oneFiled){
+      setDisplayValue(String(firstValue / 100))
+      setFirstValue(firstValue / 100)
+    }else{
+      setSecondValue((firstValue * secondValue) / 100)
+    }
+  }
+
+  function addOperation(operator){
+    if(!oneFiled){
+      setOperation(operator)
+      setOne(true)
+    }else{
+      setOperation(operator)
     }
   }
 
@@ -140,9 +149,9 @@ export default function App() {
             { displayValue }
           </div>
           <div className='buttons'>
-            <button className='item item1 clear' onClick={ () => removeValues()}>{ clean }</button>
-            <button value='+/-' className='item item1 negative' onClick={e => changeNumbersVariations(e.target.value)}>+/-</button>
-            <button value='%' className='item percent sign' onClick={e => changeNumbersVariations(e.target.value)}>%</button>
+            <button className='item item1 clear' id='cleanValue' onClick={() => removeValues()}>{ displayValue !== '0' ? 'C' : 'AC' }</button>
+            <button value='+/-' className='item item1 negative' onClick={() => changePositiveOrNegative()}>+/-</button>
+            <button value='%' className='item percent sign' onClick={() => porcentageValue()}>%</button>
             <button value='/' className='item item3 sign' onClick={e => addOperation(e.target.value)}>&divide;</button>
             <button value='7' className='item numbers' onClick={e => addValue(e.target.value)}>7</button>
             <button value='8' className='item numbers' onClick={e => addValue(e.target.value)}>8</button>
@@ -157,7 +166,7 @@ export default function App() {
             <button value='3' className='item numbers' onClick={e => addValue(e.target.value)}>3</button>
             <button value='+' className='item item3 sign' onClick={e => addOperation(e.target.value)}>+</button>
             <button value='0' className='item numbers zero' onClick={e => addValue(e.target.value)}>0</button>
-            <button value=',' className='item comma'>,</button>
+            <button value='.' className='item comma' onClick={e => addValue(e.target.value)}>,</button>
             <button value='=' className='item item3 equals' onClick={e => getResult(e.target.value)}>=</button>
           </div>
       </div>
